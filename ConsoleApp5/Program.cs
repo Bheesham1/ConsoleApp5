1dd437c0a58d9ac8f589d174ab5d45c5c88e2413
visualstudio <visualstudio@230-PC36>
2019-03-08 14:46:21 -0500

    lab15

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,38 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace ConsoleApp5
+namespace DelegatesAndEvents
 {
+    using System;
+
+    delegate void ExampleDelegate(string xyz);
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Method1(string xyz)
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
+            Console.WriteLine(xyz + " Method1");
+        }
+
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
             Console.ReadLine();
         }
     }
-    public delegate void MyDelegate(ref int intValue);
-
-    public class DelegateExercises
-    {
-        void Method1(ref int intValue)
-        {
-            intValue = intValue + 5;
-            System.Console.WriteLine("Method1 " + intValue);
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intParameter = 5;
-            myDelegate2(ref intParameter);
-        }
-    }
-
 }
