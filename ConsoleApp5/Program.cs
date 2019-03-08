0f0a379023dae07b1caa0eab73dc9b3760931f03
visualstudio <visualstudio@230-PC36>
2019-03-08 14:40:34 -0500

    lab11

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -16,24 +16,4 @@ namespace ConsoleApp5
         }
     }
 
-    public delegate void MyDelegate();
-
-    public class DelegateExercises
-    {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-
-
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
-        }
-    }
-
-
-}
+     
