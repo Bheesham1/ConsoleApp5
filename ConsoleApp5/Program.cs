4be18b242cb76d00091358d4adc0264ed7651304
visualstudio <visualstudio@230-PC36>
2019-03-08 14:23:01 -0500

    lab 1

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,44 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DelegatesAndEvents
-    using System;
-
-namespace DelegatesAndEvents
+namespace ConsoleApp5
 {
-
-    public class DelegateExercises
+    class Program
     {
-        public delegate int MyDelegate(int intValue);
-
-        public int Method1(int intMethod1)
+        static void Main(string[] args)
         {
-            return intMethod1 * 2;
-        }
-
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
         }
     }
-
-    public class Program
+    public class DelegateExercises
     {
-        public static void Main()
+        public delegate void MyDelegate();
+        void Method1()
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
+            Console.WriteLine("Method1");
             Console.ReadLine();
-
         }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+
     }
 }
