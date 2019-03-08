e4bfe9bbfafda0037df88c70cdf3c79c81491878
visualstudio <visualstudio@230-PC36>
2019-03-08 14:43:39 -0500

    lab15

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -15,23 +15,19 @@ namespace ConsoleApp5
             Console.ReadLine();
         }
     }
-    public delegate int MyDelegate(out int i);
+    public delegate void MyDelegate();
 
     public class DelegateExercises
     {
-        int Method1(out int i)
+        void Method1()
         {
-            i = 100;
-            System.Console.WriteLine("Method1 " + i);
-            return 0;
+            throw new System.Exception();
         }
+
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = null;
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
+            myDelegate();
         }
     }
 
